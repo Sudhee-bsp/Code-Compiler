0ea1865a495bbab6473d495a5241dce8da381c58
Sudhee-bsp <sudheendrapai.b@gmail.com>
2022-01-28 23:06:54 +0530

    compiler v1.0

---
+++ mycompiler.php
@@ -0,0 +1,254 @@
+<?php
+
+	declare(strict_types=1);
+
+	require_once realpath(__DIR__ . '/vendor/autoload.php');
+
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+	$dotenv->load();
+
+	$code = $stdin = $language = $memory = $cpuTime = $output = "";
+
+	if(isset($_POST['getoutput'])){
+		$code = $_POST['code'];
+		$stdin = $_POST['stdin'];
+		$language = $_POST['lang'];
+
+		// API URL
+		$url = 'https://api.jdoodle.com/v1/execute';
+
+		// Create a new cURL resource
+		$ch = curl_init($url);
+		
+		// Setup request to send json via POST
+		$data = array(
+			// 'code'=>
+			// 'import java.util.*;
+			// import java.util.regex.*;
+			
+			// public class Main2 
+			// {
+			// 	public static boolean check(Pattern pattern,Matcher m)
+			// 	{
+			// 		boolean val=false;
+			// 		if(m.find())
+			// 		{
+			// 			val=true;
+			// 		}
+			// 		else{
+			// 			val=false;
+			// 		}
+			// 		return val;
+			// 	}
+			// 	public static void main(String[] args) 
+			// 	{
+			// 		Scanner in=new Scanner(System.in);
+			// 		System.out.println("Enter fleet of soldiers");
+			// 		String sen=in.nextLine();
+					
+					
+			// 		System.out.println("Enter pattern");
+			// 		String pat=in.nextLine();
+			// 		Pattern pattern = Pattern.compile(pat);
+			// 		Matcher m = Pattern.compile(pat).matcher(sen);
+					
+			// 		while(check(pattern,m))
+			// 		{
+			// 			sen = sen.replaceFirst(pat,"");
+			// 			m=Pattern.compile(pat).matcher(sen);
+			// 		}
+			// 		if(sen.isEmpty())
+			// 		{
+			// 			System.out.println(10);
+			// 		}
+			// 		else{
+			// 			System.out.println(sen);
+			// 		}
+			// 	}
+			// }
+			// ',
+
+			// "clientId" => "1dd6b5d11c188278d50f365831663577",
+			// "clientSecret" => "c1bde6e978a34732b735980a019e3b931fe96f7573c428bf892ee325e57a0061",
+
+			"clientId" => $_ENV['CLIENT_ID'],
+			"clientSecret" => $_ENV['CLIENT_SECRET'],
+			'script' => $code,
+			'stdin' => $stdin,
+			'language' => $language,
+			'versionIndex' => 0
+		);
+
+		
+		$payload = json_encode($data);
+
+		// Attach encoded JSON string to the POST fields
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+		// Set the content type to application/json
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+		// Return response instead of outputting
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		// Execute the POST request
+		$result = curl_exec($ch);
+
+		// Close cURL resource
+		curl_close($ch);
+
+
+		$response = json_decode($result, true);
+		$output = $response['output'];
+		
+
+		$memory = $response['memory'];
+		
+
+		$cpuTime = $response['cpuTime'];
+	}
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="./style.css" />
+</head>
+<body>
+	<div class="container"> 
+		<div class=" text-center mt-3 ">
+        	<h1>&lt;My Code Compiler /&gt;</h1>
+    	</div>
+		<div class="row ">
+			<div class="col-lg-12 mx-auto">
+				<div class="card mt-2 mx-auto p-4 bg-light">
+					<div class="card-body bg-light">
+						<div class="container">
+							<form action="./mycompiler.php" method="POST" id="contact-form" role="form">
+								<div class="controls">
+								
+									<div class="row">
+										<div class="col-md-4">
+											<div class="form-group"> 
+												<label for="form_need">Choose language</label> 
+												<select id="lang" name="lang" class="form-control" required="required">
+													<option value="java"
+													<?php if(isset($_POST['lang']) && $_POST['lang'] == 'java') 
+														echo ' selected="selected"';
+													?>
+													>Java</option>
+													<option value="python3"
+													<?php if(isset($_POST['lang']) && $_POST['lang'] == 'python3') 
+														echo ' selected="selected"';
+													?>
+													>Python3</option>
+													<option value="c"
+													<?php if(isset($_POST['lang']) && $_POST['lang'] == 'c') 
+														echo ' selected="selected"';
+													?>
+													>C</option>
+												</select> 
+											</div>
+										</div>
+									</div>
+
+									<div class="row">
+										<div class="col-md-12">
+											<div class="form-group"> 
+												<label for="form_message">Your Code </label> 
+												<textarea id="form_message" name="code" class="form-control" placeholder="Write your code here." rows="10" col="100" autocomplete="on" required="required"><?php if(isset($_POST['code'])){echo $code;} ?></textarea> 
+											</div>
+											<br>
+											<label for="form_inputs">Inputs: (if any)</label> 
+											<br/>
+											<textarea id="form_inputs" name="stdin" rows="5" cols="60"></textarea>
+											<br><br>
+										</div>
+										<div class="col-md-12"> 
+											<input name="getoutput" type="submit" class="btn btn-success btn-send pt-2 btn-block " value="Run"> 
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div> <!-- /.8 -->
+			</div> <!-- /.row-->
+		</div>
+	</div>
+
+	<!-- ----------------------------output div---------------------- -->
+	<div class="container"  id="outputdiv">
+		<div class="text-left mt-5 ">
+        	<h4>Your output:</h4>
+    	</div>
+		
+		<div class="row">
+			<div class="col-lg-12 mx-auto">
+				<div class="card mt-2 mx-auto p-4 bg-light">
+					<div class="card-body bg-light">
+						<textarea class="form-control" name="stdin" rows="4" cols="70" readonly><?php echo $output; ?></textarea>
+						<br/>
+						<br/>
+						<h6>Memory: <?php echo $memory; ?></h6>
+						<h6>CPU time: <?php echo $cpuTime; ?>s</h6>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<br><br><br><br>
+	<!-- ------------------------------------------------------------------- -->
+</body>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+if ( window.history.replaceState ) {
+  window.history.replaceState( null, null, window.location.href );
+}
+
+document.getElementById('form_message').addEventListener('keydown', function(e) {
+  if (e.key == 'Tab') {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.value = this.value.substring(0, start) +
+      "\t" + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart =
+      this.selectionEnd = start + 1;
+  }
+});
+
+(function(){
+    function insertInto(str, input){
+        var val = input.value, s = input.selectionStart, e = input.selectionEnd;
+        input.value = val.slice(0,e)+str+val.slice(e);
+        if (e==s) input.selectionStart += str.length - 1;
+        input.selectionEnd = e + str.length -1;
+    }
+    var closures = {40:')',91:']', 123:'}', 39:"'", 34:'"'};
+    $(".form-control").keypress(function(e) {
+        if (c = closures[e.which]) insertInto(c, this);
+    });
+})();
+
+
+<?php
+	if(isset($_POST['getoutput'])){
+?>
+	$(function() {
+		$('html, body').animate({
+			scrollTop: $("#outputdiv").offset().top
+		}, 1000);
+		});
+<?php
+	}
+?>
+</script>
+</html>
